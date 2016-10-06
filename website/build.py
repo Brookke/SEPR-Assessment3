afc4c7507f17c7e530d1ee0366afeedc945fc7e0
Peter Ryan Smith <comprs@outlook.com>
2016-10-06 17:53:13 +0100

    Start work on the website

---
+++ website/build.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os, os.path
+from jinja2 import Environment, FileSystemLoader
+
+jinja_extension = ".jinja"
+
+output_files = [
+    "index.xhtml",
+    "designdocs/index.xhtml",
+]
+
+root_dir = "https://comprs.github.io/TeamFarce-InitialRepo/"
+
+template_dir = "./templates/"
+output_dir = "./output/"
+
+def prepare_output_dir():
+    for output_file in output_files:
+        destination_file = os.path.join(output_dir, output_file)
+        needed_dir = os.path.dirname(destination_file)
+        os.makedirs(needed_dir, exist_ok = True)
+
+if __name__ == "__main__":
+    prepare_output_dir()
+    env = Environment(loader = FileSystemLoader(template_dir))
+    for output_file in output_files:
+        template_location = output_file + jinja_extension
+        template = env.get_template(template_location)
+        output_string = template.render(root_dir = root_dir)
+        with open(os.path.join(output_dir, output_file), "w") as f:
+            f.write(output_string)
