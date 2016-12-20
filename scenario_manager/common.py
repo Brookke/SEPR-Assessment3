bda99b5d4ef1e7bb4bcfc69daa8ab471d9be9eb7
Peter Ryan Smith <ps1011@york.ac.uk>
2016-12-20 18:53:34 +0000

    Create an database resource form base class

--- scenario_manager/common.py
+++ scenario_manager/common.py
@@ -1,10 +1,61 @@
 #!/usr/bin/env python3
 
 import os.path
+from abc import ABCMeta, abstractmethod
 from PyQt5.QtCore import Qt
-from PyQt5 import QtGui
+from PyQt5 import QtCore, QtWidgets, QtGui
 
+# This is the location of the "*.ui" files. These are in the directory "ui_forms" which is assumed
+# to be in the same directory as this file. If either this file changes location or the location of
+# "*.ui" files changes, this constant should be updated.
 SOURCE_LOCATION = os.path.join(os.path.dirname(__file__), "ui_forms/")
+
+class QtABCMeta(QtCore.pyqtWrapperType, ABCMeta):
+    """This class is to stop metaclass conflicts.
+
+    These conflicts happen when trying to create an abstract base class that is derived by a class
+    which derives one of PyQt's types. See the following web page for more information:
+    https://stackoverflow.com/a/28727066
+    """
+
+    pass
+
+class DatabaseResourceForm(QtWidgets.QWidget, metaclass = QtABCMeta):
+    """This class is designed to be the base class of the widgets used in the tab widget of the
+    main application window.
+
+    This class has functionality to allow the form to respond to a database change or a resource
+    root change. The two methods which handle this have default implementations. However, a model
+    reset is needed when the database or resource root changes; the method which handles this is
+    abstract and therefore must be implemented by the child class. This is done so that the form
+    can re-instantiate the model it needs to use.
+
+    Attributes:
+        database: This is the current database in use by this form.
+        resource_root: This is the current resource root in use by this form.
+        model: This is the current model in use by this form.
+    """
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.database = None
+        self.resource_root = None
+        self.model = None
+
+    def set_database(self, database):
+        """Set a new database to interact with."""
+        self.database = database
+        self.reset_model()
+
+    def set_resource_root(self, resource_root):
+        """Set a new resource root to use internally."""
+        self.resource_root = resource_root
+        self.reset_model()
+
+    @abstractmethod
+    def reset_model(self):
+        """Adapts the form to a change of model or resource root."""
+        pass
 
 def resource_icon(model, index, resource_root = None):
     # The filename of the icon we are to load should be the same data in the database
--- scenario_manager/form_resource.py
+++ scenario_manager/form_resource.py
@@ -4,56 +4,39 @@ import os.path
 import PyQt5.uic
 from PyQt5.QtCore import Qt
 from PyQt5 import QtCore, QtWidgets, QtGui, QtSql
-from common import resource_icon, SOURCE_LOCATION
+from common import resource_icon, SOURCE_LOCATION, DatabaseResourceForm
 
 RESOURCE_FORM_SOURCE = os.path.join(SOURCE_LOCATION, "form_resource.ui")
 UiResourceFrom = PyQt5.uic.loadUiType(RESOURCE_FORM_SOURCE)[0]
 
-class ResourceForm(UiResourceFrom, QtWidgets.QWidget):
+class ResourceForm(UiResourceFrom, DatabaseResourceForm):
     """This class represents the form for manipulating the resources."""
 
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setupUi(self)
 
-        self._database = None
-        self._resource_root = "./"
-        self._model = None
-
-    def set_database(self, database):
-        """Set a new database to interact with."""
-
-        self._database = database
-        self._reset_model()
-
-    def set_resource_root(self, resource_root):
-        """Set a new resource root to use internally."""
-
-        self._resource_root = resource_root
-        self._reset_model()
-
-    def _reset_model(self):
-        # Adapts the form to a change of model or resource root.
-        if self._database:
-            self._model = ResourceModel(self, self._database, self._resource_root)
-            self.resource_list.setModel(self._model)
+    def reset_model(self):
+        if self.database:
+            self.model = ResourceModel(self, self.database, self.resource_root)
+            self.resource_list.setModel(self.model)
             self.resource_list.setModelColumn(1)
         else:
             self.resource_list.setModel(None)
 
     @QtCore.pyqtSlot()
     def add_resource(self):
-        if self._model:
-            new_index = self._model.rowCount()
-            self._model.insertRows(new_index, 1)
-            self.resource_list.edit(self._model.index(new_index, 1))
+        if self.model:
+            new_index = self.model.rowCount()
+            self.model.insertRows(new_index, 1)
+            self.resource_list.edit(self.model.index(new_index, 1))
 
     @QtCore.pyqtSlot()
     def remove_resources(self):
-        if self._model:
+        if self.model:
             for index in self.resource_list.selectedIndexes():
-                self._model.removeRow(index.row(), index.parent())
-            self._model.select()
+                self.model.removeRow(index.row(), index.parent())
+            self.model.select()
 
 class ResourceModel(QtSql.QSqlTableModel):
     """This class represents the database table resources."""
@@ -63,14 +46,14 @@ class ResourceModel(QtSql.QSqlTableModel):
         self.setTable("resources")
         self.setEditStrategy(QtSql.QSqlTableModel.OnFieldChange)
         self.select()
-        self._resource_root = resource_root
+        self.resource_root = resource_root
 
     def data(self, index, role = Qt.DisplayRole):
         # This method is overridden in order to provide icons of the resources themselves. This is
         # done by providing a valid icon for the role "DecorationRole".
 
         if index.column() == 1 and role == Qt.DecorationRole:
-            return resource_icon(self, index, self._resource_root)
+            return resource_icon(self, index, self.resource_root)
         else:
             # Don't touch the other columns/roles. Delegate this to the parent.
             return super().data(index, role)
