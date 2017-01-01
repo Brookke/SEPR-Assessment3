610fca59735f8740605a1467f99e572d3665d489
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-01 23:22:39 +0000

    Fix up the documentation in the common module

--- scenario_manager/common.py
+++ scenario_manager/common.py
@@ -58,17 +58,21 @@ class DatabaseResourceForm(QtWidgets.QWidget, metaclass = QtABCMeta):
         pass
 
 def resource_icon(model, index, resource_root = None):
-    # The filename of the icon we are to load should be the same data in the database
-    # itself. We can extract this information from the model by using the very function
-    # we are in; just by modifying the intended role but maintaining the index.
+    """Retrieve an icon for the given model, index and resource root. This will use the path data
+    in the same model and index to infer the path of the icon (which will be the resource itself).
+    """
+
+    # The filename of the icon we are to load should be the same data in the database itself. We
+    # can extract this information from the model by using the same index but just modifying the
+    # intended role.
     stored_filename = model.data(index, Qt.EditRole)
     if not stored_filename:
         return None
 
-    # We can try three different methods to find the file to load. The first method is to
-    # prepend the resource root (the primary use of this variable). The second is to load
-    # the file with the filename, as is. The last is to prepend "./". We do not consider
-    # the first method if the resource root variable is invalid.
+    # We can try three different methods to find the file to load. The first method is to prepend
+    # the resource root (the primary use of this variable). The second is to load the file with
+    # the filename, as is. The last is to prepend "./". We do not consider the first method if the
+    # resource root variable is invalid.
     icon_filenames = [
         stored_filename,
         os.path.join("./", stored_filename),
@@ -76,7 +80,7 @@ def resource_icon(model, index, resource_root = None):
     if resource_root:
         icon_filenames.insert(0, os.path.join(resource_root, stored_filename))
 
-    # Check the validity of the filenames be checking to see if the file exits.
+    # Check the validity of the filenames by checking to see if the file exits.
     valid_icon_filenames = list(filter(os.path.exists, icon_filenames))
     if len(valid_icon_filenames) == 0:
         return None
