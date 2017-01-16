67bd49d86fe04d5ad8c210ba57371f92d2e81d7c
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-16 22:27:30 +0000

    Fix the formatting for Clue

--- game/src/main/java/org/teamfarce/mirch/Clue.java
+++ game/src/main/java/org/teamfarce/mirch/Clue.java
@@ -2,16 +2,16 @@ package org.teamfarce.mirch;
 
 /**
  * Stores information about a single clue.
- * @author jacobwunwin
  *
+ * @author Jacob Wunwin
  */
 public class Clue {
     int provesMotive;
     int provesMean;
 
     /**
-     * Initialiser function
+     * Initialiser function.
      */
-    Clue(){
+    Clue() {
     }
 }
