7c929f7a548a6848a26d5d6df6c122ea5193d196
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-16 22:08:47 +0000

    Fix the formatting for DialogueTree

--- game/src/main/java/org/teamfarce/mirch/DialogueTree.java
+++ game/src/main/java/org/teamfarce/mirch/DialogueTree.java
@@ -3,27 +3,28 @@ package org.teamfarce.mirch;
 import java.util.ArrayList;
 
 /**
- * PLEASE FILL IN THIS EXPLANATION
- * @author jacobwunwin
+ * PLEASE FILL IN THIS EXPLANATION.
  *
+ * @author Jacob Wunwin
  */
 public class DialogueTree {
     ArrayList<QuestioningIntention> questionIntentions;
     ArrayList<QuestioningIntention> askedQuestions;
 
     /**
-     * Initialiser function
+     * Initialiser function.
      */
     DialogueTree(){
     }
 
     /**
-     * Returns a new Dialogue from the given intention and style
-     * @param intention
-     * @param style
-     * @return Dialogue
+     * Returns a new Dialogue from the given intention and style.
+     *
+     * @param intention The id of the intention of the question
+     * @param style The id of the style of the question
+     * @return The response dialogue
      */
-    Dialogue askQuestion(int intention, int style){
+    Dialogue askQuestion(int intention, int style) {
         return new Dialogue();
     }
 }
