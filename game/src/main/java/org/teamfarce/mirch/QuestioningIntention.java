bd2c0dacf2707d1a2cfdd016905d049715f20fd9
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-16 22:13:14 +0000

    Fix the formatting for QuestioningIntention

--- game/src/main/java/org/teamfarce/mirch/QuestioningIntention.java
+++ game/src/main/java/org/teamfarce/mirch/QuestioningIntention.java
@@ -3,26 +3,27 @@ package org.teamfarce.mirch;
 import java.util.ArrayList;
 
 /**
- * PLEASE FILL IN THIS EXPLANATION
- * @author jacobwunwin
+ * PLEASE FILL IN THIS EXPLANATION.
  *
+ * @author Jacob Wunwin
  */
 public class QuestioningIntention {
     ArrayList<Question> questions;
     String description;
 
     /**
-     * Initialiser function
+     * Initialiser function.
      */
     QuestioningIntention(){
     }
 
     /**
-     * Allows the asking of a question in a given style, returning a QuestionResult
-     * @param style
-     * @return questionResult
+     * Allows the asking of a question in a given style, returning a QuestionResult.
+     *
+     * @param style The id of the questioning style to use
+     * @return The concrete question to be asked
      */
-    QuestionResult askQuestion(int style){
+    QuestionResult askQuestion(int style) {
         return new QuestionResult();
     }
 }
