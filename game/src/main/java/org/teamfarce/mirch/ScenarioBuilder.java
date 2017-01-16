65d2df0ef05894aeab043524d131bd6cd965707e
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-16 21:45:54 +0000

    Fix the formatting for ScenarioBuilder

--- game/src/main/java/org/teamfarce/mirch/ScenarioBuilder.java
+++ game/src/main/java/org/teamfarce/mirch/ScenarioBuilder.java
@@ -4,72 +4,73 @@ import java.util.ArrayList;
 
 public class ScenarioBuilder {
     /**
+     * Generates an ArrayList of suspects and returns the ArrayList.
      *
      * @return ArrayList of Suspects
-     *
-     * Generates an ArrayList of suspects and returns the ArrayList
      */
-    public ArrayList<Suspect> generateSuspects(){
+    public ArrayList<Suspect> generateSuspects() {
         return null;
     }
 
     /**
+     * Chooses a suspect from the suspect list and returns the suspect object.
      *
-     * @param suspectList
+     * @param suspectList The list of potential victims
      * @return Suspect
-     *
-     * Chooses a suspect from the suspect list and returns the Suspect Object
      */
-    public Suspect chooseVictim(ArrayList<Suspect> suspectList){
+    public Suspect chooseVictim(ArrayList<Suspect> suspectList) {
         return null;
     }
 
     /**
+     * Randomly selects a murderer from the suspect list, marks that suspect and then returns the
+     * edited list.
      *
-     * @param suspectList
+     * @param suspectList The list of potential murderers
      * @return updates suspectList with murderer marked
-     *
-     * Randomly selects a murderer from the suspect list, and marks that suspect
-     * Then returns the edited list
      */
-    public ArrayList<Suspect> chooseMurderer(ArrayList<Suspect> suspectList){
+    public ArrayList<Suspect> chooseMurderer(ArrayList<Suspect> suspectList) {
         return suspectList;
     }
 
     /**
+     * Generates an array list of rooms of size 'size' and returns the list.
      *
-     * @param size
+     * @param size The number of rooms to generate
      * @return ArrayList of rooms
-     *
-     * Generates an array list of rooms of size 'size' and returns the list
      */
-    public ArrayList<Room> generateRooms(int size){
+    public ArrayList<Room> generateRooms(int size) {
         return null;
     }
 
     /**
+     * Takes an ArrayList of props and a room ID, adds additional props to the list and returns the
+     * extended list.
      *
-     * @param props
-     * @param roomID
+     * @param props The props which can be added into the room
+     * @param roomId The room to add props to
      * @return ArrayList of Props
-     *
-     * Takes an ArrayList of props and a room ID, adds additional props to the list and returns the extended list
      */
-    public ArrayList<Prop> generateProps(ArrayList<Prop> props, int roomID){
+    public ArrayList<Prop> generateProps(ArrayList<Prop> props, int roomId) {
         return null;
     }
 
     /**
+     * Generates a clue object and returns it.
      *
      * @return Clue
-     *
-     * Generates a clue object and returns it
      */
-    public Clue generateClue(){
+    public Clue generateClue() {
         return null;
     }
 
-    public GameSnapshot generateGame(int roomNumber/*Pass character traits here*/){
+    /**
+     * Generate the initial game state.
+     *
+     * @param roomNumber The number of rooms to generate
+     * @return The generated initial game state
+     */
+    public GameSnapshot generateGame(int roomNumber/*Pass character traits here*/) {
         //Generate suspects
         ArrayList<Suspect> suspects = this.generateSuspects();
 
@@ -78,7 +79,7 @@ public class ScenarioBuilder {
 
         //Remove victim from suspect list
         boolean success = suspects.remove(victim);
-        if (success){
+        if (success) {
             System.out.println("Suspect removal successful");
         } else {
             System.out.println("Suspect removal failed");
@@ -96,13 +97,13 @@ public class ScenarioBuilder {
         ArrayList<Prop> props = null; //create an empty list of props
 
         //Iterate through each room in the rooms ArrayList
-        for (Room room: rooms){
+        for (Room room: rooms) {
             props = this.generateProps(props, room.id);
         }
 
         //For Each Prop, generate Clues
         //Iterate through each prop in the props ArrayList
-        for (Prop prop : props){
+        for (Prop prop : props) {
             prop.clue.add(this.generateClue()); //add a clue to the prop
         }
 
@@ -110,7 +111,7 @@ public class ScenarioBuilder {
 
         //Generate Dialogue Tree
         //Iterate through each suspect in the suspect list
-        for (Suspect suspect : suspects){
+        for (Suspect suspect : suspects) {
             //generate a dialogue tree for the suspect
         }
 
