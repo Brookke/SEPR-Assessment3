a22686a790aea4c84fb36d441d20730da4ba800b
Jacob Unwin <jacobsax@gmail.com>
2017-01-18 13:55:14 +0000

    added visual components
    added visual components, spawned sprite

--- game/src/main/java/org/teamfarce/mirch/Application.java
+++ game/src/main/java/org/teamfarce/mirch/Application.java
@@ -1,9 +1,15 @@
 package org.teamfarce.mirch;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.math.Vector2;
 
 class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "MIRCH";
+        config.width = 1366;
+        config.height = 768;
+        new LwjglApplication(new MIRCH(), config);
     }
 }
