b2d311a1920444b162a5ce075646fcac6efa0072
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-22 17:30:27 +0000

    Upgrade the room class.

--- game/src/main/java/org/teamfarce/mirch/Room.java
+++ game/src/main/java/org/teamfarce/mirch/Room.java
@@ -4,20 +4,42 @@ import com.badlogic.gdx.math.Vector2;
 
 /**
  * Represents a room in the map.
- *
- * @author Jacob Wunwin
  */
 public class Room {
-    Vector2 position;
-    Vector2 size;
-    int id;
-    String filename;
+    /**
+     * This is the amount of pixels for a single room unit.
+     */
+    public static final float pixelsPerUnit = 160.0f;
+
+    /**
+     * The position of origin of this room
+     */
+    public Vector2 position;
+
+    /**
+     * The filename of the image for the background of this room.
+     */
+    public String filename;
 
     /**
      * Initialiser Function for Room Class.
+     *
+     * @param filename The filename of the background image.
+     * @param postion The position to set.
      */
-    Room(String file, Vector2 position) {
-    	this.filename = file;
-    	this.position = position;
+    Room(String filename, Vector2 position) {
+        this.filename = filename;
+        this.position = position;
+    }
+
+    /**
+     * This constructs a new room with a position in Room Units.
+     *
+     * @param filename The filename of the background image.
+     * @param position The position to scale and then set.
+     * @return The new room.
+     */
+    static Room constructWithUnitSizes(String filename, Vector2 position) {
+        return new Room(filename, position.scl(pixelsPerUnit));
     }
 }
