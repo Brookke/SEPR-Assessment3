a9b9789d43042a4c62aacd72c7e8f0a6e00b3640
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-23 16:11:38 +0000

    Clean up DisplayController

--- game/src/main/java/org/teamfarce/mirch/DisplayController.java
+++ game/src/main/java/org/teamfarce/mirch/DisplayController.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.teamfarce.mirch;
 
 import java.util.ArrayList;
@@ -15,120 +12,140 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /**
  * The display controller controls the drawing of all items/objects/GUI elements to the display.
+ * <p>
  * It also controls the handling of GUI element inputs
- * @author jacobwunwin
- *
+ * </p>
  */
 public class DisplayController {
-	private SpriteBatch batch;
-	private DisplayMap displayMap;
-	private GUIController guiController;
-	private Skin uiSkin;
-	private GameSnapshot gameSnapshot;
-	
-	
-	/**
-	 * Controls the initial character traits selection at the start of the game.
-	 * Selection is made through a series of pop up windows.
-	 * Returns an aray of integers containing the trait selections.
-	 * @return
-	 */
-	private int[] drawCharacterSelection(){
-		int[] values  = new int[3];
-		String backstory = "Intro";
-		JOptionPane.showMessageDialog(null,backstory, "Character Creation",JOptionPane.PLAIN_MESSAGE);
-		Object[] options1 = {"Aggresive",
-                "Neutral",
-                "Passive"};
-		Object[] options2 = {"Ruthless",
-                "Neutral",
-                "Kind"};
-		Object[] options3 = {"Perceptive",
-                "Crazy",
-                "Evil"};
-		values[0] = JOptionPane.showOptionDialog(null,
-						"How agressive is your character?",
-						"Character Creation",
-						JOptionPane.YES_NO_CANCEL_OPTION,
-						JOptionPane.QUESTION_MESSAGE,
-						null,
-						options1,
-						options1[2]);
-		values[1] = JOptionPane.showOptionDialog(null,
-						"How compassionate is your character?",
-						"Character Creation",
-						JOptionPane.YES_NO_CANCEL_OPTION,
-						JOptionPane.QUESTION_MESSAGE,
-						null,
-						options2,
-						options2[2]);
-		values[2] = JOptionPane.showOptionDialog(null,
-						"Choose a defining trait for your character",
-						"Character Creation",
-						JOptionPane.YES_NO_CANCEL_OPTION,
-						JOptionPane.QUESTION_MESSAGE,
-						null,
-						options3,
-						options3[2]);
-		return values;
-	}
-	
+    private DisplayMap displayMap;
+    private GUIController guiController;
+    private Skin uiSkin;
 
+    /**
+     * Initialises the DisplayController.
+     *
+     * @param skin The skin to use for the ui.
+     * @param gSnapshot The game snapshot to use.
+     * @param batch The spitebatch to pass to the gui controller.
+     */
+    DisplayController(Skin skin, GameSnapshot gSnapshot, SpriteBatch batch) {
+        this.displayMap = new DisplayMap(batch);
+        this.uiSkin = skin;
+        this.guiController = new GUIController(uiSkin, gSnapshot, batch);
+    }
 
-	/**
-	 * Creates a pop up window with text announcing that the user has found the referenced prop
-	 * @param prop
-	 */
-	public void drawItemDialogue(Prop prop){
-		String output;
-		output = "You find: '" + prop.description + "'. It has been added to your Journal.";
-		JOptionPane.showMessageDialog(null,output, prop.name,JOptionPane.PLAIN_MESSAGE);
-	}
-	
-	/**
-	 * Creates a pop up window with text announcing that the user has already found the referenced prop
-	 * @param prop
-	 */
-	public void drawItemAlreadyFoundDialogue(Prop prop){
-		String output;
-		output = "You find: '" + prop.description + "'. You've already found this item.";
-		JOptionPane.showMessageDialog(null,output, prop.name,JOptionPane.PLAIN_MESSAGE);
-	}
-	
-	/**
-	 * Initialises the DisplayController
-	 * @param skin
-	 * @param gSnapshot
-	 * @param batch
-	 */
-	DisplayController(Skin skin, GameSnapshot gSnapshot, SpriteBatch batch){
-		this.batch = batch;
-		this.displayMap = new DisplayMap(batch);
-		this.uiSkin = skin;
-		this.gameSnapshot = gSnapshot;
-		this.guiController = new GUIController(uiSkin, gameSnapshot, this.batch);
-	}
-	
-	/**
-	 * Draws the map to the screen
-	 * @param rooms
-	 * @param doors
-	 * @param objects
-	 * @param characters
-	 */
-	void drawMap(ArrayList<RenderItem> rooms, ArrayList<RenderItem> doors, ArrayList<RenderItem> objects, ArrayList<RenderItem> characters){
-		this.displayMap.drawMap(rooms, doors, objects, characters);
-		this.guiController.drawControlStage();
-		Gdx.input.setInputProcessor(this.guiController.controlStage);
-	}
-	
-	/**
-	 * Returns the drawGUI object
-	 * @return
-	 */
-	GUIController drawGUI(){
-		return this.guiController;
-	}
-	
+    /**
+     * Controls the initial character traits selection at the start of the game.
+     * <p>
+     * Selection is made through a series of pop up windows.
+     * </p>
+     *
+     * @return An array of integers containing the trait selections.
+     */
+    private int[] drawCharacterSelection() {
+        int[] values  = new int[3];
+        String backstory = "Intro";
+        JOptionPane.showMessageDialog(null, backstory, "Character Creation",JOptionPane.PLAIN_MESSAGE);
+        Object[] options1 = {
+            "Aggresive",
+            "Neutral",
+            "Passive"
+        };
+        Object[] options2 = {
+            "Ruthless",
+            "Neutral",
+            "Kind"
+        };
+        Object[] options3 = {
+            "Perceptive",
+            "Crazy",
+            "Evil"
+        };
+        values[0] = JOptionPane.showOptionDialog(
+            null,
+            "How agressive is your character?",
+            "Character Creation",
+            JOptionPane.YES_NO_CANCEL_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options1,
+            options1[2]
+        );
+        values[1] = JOptionPane.showOptionDialog(
+            null,
+            "How compassionate is your character?",
+            "Character Creation",
+            JOptionPane.YES_NO_CANCEL_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options2,
+            options2[2]
+        );
+        values[2] = JOptionPane.showOptionDialog(
+            null,
+            "Choose a defining trait for your character",
+            "Character Creation",
+            JOptionPane.YES_NO_CANCEL_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null,
+            options3,
+            options3[2]
+        );
+        return values;
+    }
 
+    /**
+     * Creates a pop up window with text announcing that the user has found the referenced prop.
+     *
+     * @param prop The prop to draw the dialogue for.
+     */
+    public void drawItemDialogue(Prop prop) {
+        String output;
+        output = String.format(
+            "You find: \"%1$s\". It has been added to your journal.", prop.description
+        );
+        JOptionPane.showMessageDialog(null, output, prop.name, JOptionPane.PLAIN_MESSAGE);
+    }
+
+    /**
+     * Creates a pop up window with text announcing that the user has already found the referenced
+     * prop.
+     *
+     * @param prop The prop to create a window.
+     */
+    public void drawItemAlreadyFoundDialogue(Prop prop) {
+        String output;
+        output = String.format(
+            "You find: \"%1$s\". You've already found the item.", prop.description
+        );
+        JOptionPane.showMessageDialog(null, output, prop.name, JOptionPane.PLAIN_MESSAGE);
+    }
+
+    /**
+     * Draws the map to the screen.
+     *
+     * @param rooms The rooms to draw.
+     * @param doors The doors to draw.
+     * @param objects The objects to draw.
+     * @param characters The characters to draw.
+     */
+    void drawMap(
+        ArrayList<RenderItem> rooms,
+        ArrayList<RenderItem> doors,
+        ArrayList<RenderItem> objects,
+        ArrayList<RenderItem> characters
+    ){
+        this.displayMap.drawMap(rooms, doors, objects, characters);
+        this.guiController.drawControlStage();
+        Gdx.input.setInputProcessor(this.guiController.controlStage);
+    }
+
+    /**
+     * Returns the drawGUI object.
+     *
+     * @return The internal gui controller.
+     */
+    GUIController drawGUI(){
+        return this.guiController;
+    }
 }
