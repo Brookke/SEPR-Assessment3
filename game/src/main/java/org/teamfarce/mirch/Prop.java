47bd83c5e0a8b8e6f97b21e7eca4a5c591712ad9
Peter Ryan Smith <ps1011@york.ac.uk>
2017-01-23 19:43:23 +0000

    Clean up prop

--- game/src/main/java/org/teamfarce/mirch/Prop.java
+++ game/src/main/java/org/teamfarce/mirch/Prop.java
@@ -5,21 +5,29 @@ import com.badlogic.gdx.math.Vector2;
 
 /**
  * Creates an individual prop entity, extends from the MapEntity class.
- *
- * @author Jacob Wunwin
  */
 public class Prop extends MapEntity {
-    List<Clue> clue;
+    private List<Clue> clues;
+
+    /**
+     * The current position of the prop in the room.
+     */
     public Vector2 roomPosition;
+
+    /**
+     * The room the prop current resides in.
+     */
     public Room currentRoom;
 
     /**
-     * Initialises the object
-     * @param dialogue
-     * @param roomPosition stores position relative to currentroom
-     * @param currentRoom a ref to the current room the suspect is in
-     * @param name a string for the name
-     * @param description a string description 
+     * Initialises the object.
+     *
+     * @param name The name for the prop.
+     * @param description A description of the prop.
+     * @param filename The filename of the image of the prop.
+     * @param roomPosition The position in the room.
+     * @param currentRoom The current room the prop is in.
+     * @param clues The clues this prop provides.
      */
     public Prop(
         String name,
@@ -27,21 +35,21 @@ public class Prop extends MapEntity {
         String filename,
         Vector2 roomPosition,
         Room currentRoom,
-        List<Clue> clue
+        List<Clue> clues
     ) {
-
         super(name, description, filename);
-        this.clue = clue;
+        this.clues = clues;
         this.roomPosition = roomPosition;
         this.currentRoom = currentRoom;
         this.filename = filename;
     }
 
     /**
-     * Alternative prop constructor mostly used for testing purposes
-     * @param filename
-     * @param room
-     * @param roomPos
+     * Alternative prop constructor mostly used for testing purposes.
+     *
+     * @param filename The filename of the image of the prop.
+     * @param room The room to start in.
+     * @param roomPos The position in the room to start at.
      */
     public Prop(String filename, Room room, Vector2 roomPos){
         super("", "", filename);
@@ -56,7 +64,7 @@ public class Prop extends MapEntity {
      * @return The list of clues
      */
     public List<Clue> takeClues() {
-        return this.clue;
+        return this.clues;
     }
 }
 
