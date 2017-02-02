9e62ddf3ac2f349e17f92911181d282537f0802b
Brooke Hatton <hatton.brooke@gmail.com>
2017-02-02 19:28:33 +0000

    Add PersonComparator

--- game/src/main/java/org/teamfarce/mirch/Entities/AbstractPerson.java
+++ game/src/main/java/org/teamfarce/mirch/Entities/AbstractPerson.java
@@ -2,6 +2,8 @@ package org.teamfarce.mirch.Entities;
 
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Comparator;
+
 /**
  * Created by brookehatton on 01/02/2017.
  */
@@ -28,5 +30,27 @@ public abstract class AbstractPerson extends MapEntity
      */
     public abstract void move(Vector2 move);
 
+
+    /**
+     * This class is to sort a list of AbstractPerson in highest Y coordinate to lowest Y coordinate
+     * <p>
+     * It is used to render NPCs and the Player in the correct order to avoid it appearing as though someone
+     * is standing on top of someone else
+     */
+    public static class PersonPositionComparator implements Comparator<AbstractPerson>
+    {
+        /**
+         * This method compares the 2 objects.
+         *
+         * @param o1 - The first object to compare
+         * @param o2 - The second object to compare
+         * @return (int) if <0 o1 is considered to be first in the list
+         */
+        @Override
+        public int compare(AbstractPerson o1, AbstractPerson o2)
+        {
+            return o2.getTileCoordinates().y - o1.getTileCoordinates().y;
+        }
+    }
 
 }
