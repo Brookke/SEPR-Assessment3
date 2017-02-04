65974b525c893092d286b1a91e856b30a73135e9
Jason Mashinchi <jasonmash@users.noreply.github.com>
2017-02-04 20:52:10 +0000

    Fixed GUIController tests

--- game/src/test/java/org/teamfarce/mirch/GUIController_Test.java
+++ game/src/test/java/org/teamfarce/mirch/GUIController_Test.java
@@ -1,16 +1,56 @@
 package org.teamfarce.mirch;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.teamfarce.mirch.Screens.AbstractScreen;
+import org.teamfarce.mirch.Screens.JournalScreen;
 
+import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
 
 /**
  * Created by Jason on 04/02/2017.
  */
-public class GUIController_Test {
+public class GUIController_Test extends GameTest {
 
     MIRCH game;
+
+    //Init empty screen for testing
+    AbstractScreen screen1 = new AbstractScreen(game) {
+        @Override
+        public void show() {}
+        @Override
+        public void render(float delta) {}
+        @Override
+        public void resize(int width, int height) {}
+        @Override
+        public void pause() {}
+        @Override
+        public void resume() {}
+        @Override
+        public void hide() {}
+        @Override
+        public void dispose() {}
+    };
+
+    AbstractScreen screen2 = new AbstractScreen(game) {
+        @Override
+        public void show() {}
+        @Override
+        public void render(float delta) {}
+        @Override
+        public void resize(int width, int height) {}
+        @Override
+        public void pause() {}
+        @Override
+        public void resume() {}
+        @Override
+        public void hide() {}
+        @Override
+        public void dispose() {}
+    };
 
     @Before
     public void init_tests()
@@ -34,10 +74,13 @@ public class GUIController_Test {
         game.gameSnapshot.setState(GameState.map);
 
         GUIController guiController = new GUIController(game);
+        guiController.journalScreen = screen1;
+        guiController.mapScreen = screen2;
         guiController.update();
 
         assertSame(guiController.currentState, GameState.map);
         assertSame(game.getScreen(), guiController.mapScreen);
+        assertNotSame(game.getScreen(), guiController.journalScreen);
     }
 
     @Test
@@ -45,9 +88,12 @@ public class GUIController_Test {
         game.gameSnapshot.setState(GameState.journalHome);
 
         GUIController guiController = new GUIController(game);
+        guiController.journalScreen = screen1;
+        guiController.mapScreen = screen2;
         guiController.update();
 
         assertSame(guiController.currentState, GameState.journalHome);
         assertSame(game.getScreen(), guiController.journalScreen);
+        assertNotSame(game.getScreen(), guiController.mapScreen);
     }
 }
