dcf5a218352cf80b2d69f27b3b63656f28181a14
Jason Mashinchi <jasonmash@users.noreply.github.com>
2017-02-05 17:26:18 +0000

    Added JournalScreen stage & background image

--- game/src/main/java/org/teamfarce/mirch/Screens/JournalScreen.java
+++ game/src/main/java/org/teamfarce/mirch/Screens/JournalScreen.java
@@ -3,8 +3,19 @@ package org.teamfarce.mirch.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import org.teamfarce.mirch.GameState;
 import org.teamfarce.mirch.MIRCH;
 import org.teamfarce.mirch.Screens.Elements.StatusBar;
 
@@ -13,19 +24,40 @@ import org.teamfarce.mirch.Screens.Elements.StatusBar;
  */
 public class JournalScreen extends AbstractScreen
 {
-    //TODO: This stuff is for testing purposes
+    private MIRCH game;
+
+    public Stage journalStage;
     private StatusBar statusBar;
 
     public JournalScreen(MIRCH game, Skin uiSkin)
     {
         super(game);
+        this.game = game;
+
+        initStage();
+
         statusBar = new StatusBar(game.gameSnapshot, uiSkin);
+    }
+
+    private void initStage() {
+        journalStage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+
+        Table table = new Table();
+        table.setBounds(80, 20, Gdx.graphics.getWidth() - 160, Gdx.graphics.getHeight() - 20);
+
+        Texture journalBackground = new Texture(Gdx.files.internal("Open_journal.png"));
+        TextureRegion tr = new TextureRegion(journalBackground);
+        TextureRegionDrawable trd = new TextureRegionDrawable(tr);
+        table.setBackground(trd);
+
+        journalStage.addActor(table);
     }
 
     @Override
     public void show()
     {
         InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(journalStage);
         multiplexer.addProcessor(statusBar.stage);
         Gdx.input.setInputProcessor(multiplexer);
     }
@@ -33,12 +65,15 @@ public class JournalScreen extends AbstractScreen
     @Override
     public void render(float delta)
     {
+        journalStage.act();
+        journalStage.draw();
         statusBar.render();
     }
 
     @Override
     public void resize(int width, int height)
     {
+        journalStage.getViewport().update(width, height, true);
         statusBar.resize(width, height);
     }
 
@@ -63,6 +98,7 @@ public class JournalScreen extends AbstractScreen
     @Override
     public void dispose()
     {
+        journalStage.dispose();
         statusBar.dispose();
     }
 }
