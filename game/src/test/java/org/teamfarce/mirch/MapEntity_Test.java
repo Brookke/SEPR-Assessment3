eb27fd2c9c2cbf5dd8bea92f60c6d0a6a8af9776
Brooke Hatton <hatton.brooke@gmail.com>
2017-02-08 10:32:48 +0000

    Add MapEntity tests back

--- game/src/test/java/org/teamfarce/mirch/MapEntity_Test.java
+++ game/src/test/java/org/teamfarce/mirch/MapEntity_Test.java
@@ -5,44 +5,49 @@ package org.teamfarce.mirch;
 
 import static org.junit.Assert.*;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import org.junit.Test;
 import org.teamfarce.mirch.Entities.MapEntity;
+import org.teamfarce.mirch.map.Room;
 
 /**
  * Test the Map Entity
  * @author jacobwunwin
  *
  */
-public class MapEntity_Test {
+public class MapEntity_Test extends GameTest {
 	
-//	@Test
-//	public void test_setRoomGetRoom(){
-//		MapEntity mapEntity = new MapEntity(null, null, null);
-//
-//		Room theRoom = new Room(null, null);
-//
-//		mapEntity.setRoom(theRoom);
-//
-//		assertEquals(theRoom, mapEntity.getRoom());
-//	}
-	
+	@Test
+	public void test_setRoomGetRoom(){
+		MapEntity mapEntity = new MapEntity(null, null,  new Texture(Gdx.files.internal("clues/Axe.png")));
+
+		Room theRoom = new Room(0, "TestMap.tmx", "test");
+		mapEntity.setRoom(theRoom);
+		assertEquals(theRoom, mapEntity.getRoom());
+	}
+
+	@Test
 	public void test_getName(){
 		String input = "Test";
-		MapEntity mapEntity = new MapEntity(input, null, null);
+		MapEntity mapEntity = new MapEntity(input, null, new Texture(Gdx.files.internal("clues/Axe.png")));
 		assertEquals(input, mapEntity.getName());
 		
 	}
-	
+
+	@Test
 	public void test_genDescription(){
-		String input = "Test";
-		MapEntity mapEntity = new MapEntity(null, input, null);
+		String input = "Test 1 2 4";
+		MapEntity mapEntity = new MapEntity(null, input, new Texture(Gdx.files.internal("clues/Axe.png")));
 		assertEquals(input, mapEntity.getDescription());
 	}
-	
+
+
+	@Test
 	public void test_getFileName(){
-		String input = "Test";
+        Texture input = new Texture(Gdx.files.internal("clues/Axe.png"));
 		MapEntity mapEntity = new MapEntity(null, null, input);
-		assertEquals(input, mapEntity.getFilename());
+		assertEquals(input, mapEntity.getTexture());
 	}
 	
 
