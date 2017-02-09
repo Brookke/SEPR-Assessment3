0a9805ab1fae34a35abfb93e8e4694034ec30d52
Joe Shuff <joe.shuff@btinternet.com>
2017-02-09 00:56:23 +0000

    Added narrator screen class... again

---
+++ game/src/main/java/org/teamfarce/mirch/Screens/NarratorScreen.java
@@ -0,0 +1,49 @@
+package org.teamfarce.mirch.Screens;
+
+import org.teamfarce.mirch.MIRCH;
+
+/**
+ * Created by joeshuff on 09/02/2017.
+ */
+public class NarratorScreen extends AbstractScreen {
+
+    public NarratorScreen(MIRCH game)
+    {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
