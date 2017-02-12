13bd3b5c4721177584e6845e608cfa025502191e
Jason Mashinchi <jason@mashinchi.com>
2017-02-12 12:50:47 +0000

    Uncommented and fixed Journal tests

--- game/src/test/java/org/teamfarce/mirch/Journal_Test.java
+++ game/src/test/java/org/teamfarce/mirch/Journal_Test.java
@@ -1,50 +1,49 @@
-///**
-// *
-// */
-//package org.teamfarce.mirch;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import org.junit.Test;
-//
-//import com.badlogic.gdx.math.Vector2;
-//
-///**
-// * Tests the journal class
-// * @author jacobwunwin
-// *
-// */
-//public class Journal_Test {
-//
-//	@Test
-//	public void test_addProp(){
-//		Journal journal = new Journal();
-//
-//		Prop prop = new Prop("myProp", null, new Vector2());
-//		journal.addProp(prop);
-//
-//		assertEquals(prop, journal.foundProps.get(0));
-//	}
-//
-//	@Test
-//	public void test_addClue(){
-//		Journal journal = new Journal();
-//
-//		Clue clue = new Clue(10, 15, "myClue");
-//
-//		journal.addClue(clue);
-//
-//		assertEquals(clue, journal.foundClues.get(0));
-//
-//	}
-//
-//	@Test
-//	public void test_getProps(){
-//		Journal journal = new Journal();
-//
-//		Prop prop = new Prop("myProp", null, new Vector2());
-//		journal.addProp(prop);
-//
-//		assertEquals(prop, journal.foundProps.get(0));
-//	}
-//}
+/**
+ *
+ */
+package org.teamfarce.mirch;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.badlogic.gdx.math.Vector2;
+import org.teamfarce.mirch.Entities.Clue;
+
+import java.util.ArrayList;
+
+/**
+ * Tests the journal class
+ * @author jacobwunwin
+ *
+ */
+public class Journal_Test extends GameTest {
+	@Test
+	public void test_addClue(){
+		Journal journal = new Journal();
+
+		Clue clue = new Clue("Clue name", "Description", 0,0,"Axe.png");
+
+		journal.addClue(clue);
+
+		assertEquals(clue, journal.foundClues.get(0));
+
+	}
+
+	@Test
+	public void test_getClues(){
+		Journal journal = new Journal();
+		ArrayList<Clue> cluesList = new ArrayList<>();
+
+		Clue clue = new Clue("Clue name", "Description", 0,0,"Axe.png");
+		Clue clue2 = new Clue("Clue name 2", "Description", 0,0,"Axe.png");
+
+		journal.addClue(clue);
+		journal.addClue(clue2);
+
+		cluesList.add(clue);
+		cluesList.add(clue2);
+
+		assertEquals(cluesList, journal.foundClues);
+	}
+}
