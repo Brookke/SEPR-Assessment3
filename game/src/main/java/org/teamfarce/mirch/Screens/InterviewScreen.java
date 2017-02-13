124818af8bf93fa9f43d4d904f16d8e587b05fd1
Jason Mashinchi <jasonmash@users.noreply.github.com>
2017-02-13 15:42:38 +0000

    Adds interview screen class

---
+++ game/src/main/java/org/teamfarce/mirch/Screens/InterviewScreen.java
@@ -0,0 +1,41 @@
+package org.teamfarce.mirch.Screens;
+
+/**
+ * Created by Jason on 13/02/2017.
+ */
+public class InterviewScreen extends AbstractScreen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
