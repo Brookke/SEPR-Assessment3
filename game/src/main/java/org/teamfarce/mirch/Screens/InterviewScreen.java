38b5a26352f1a09032a6dd0c2479b8c82c482d21
Jason Mashinchi <jasonmash@users.noreply.github.com>
2017-02-13 15:47:01 +0000

    Added basic methods and stage to interview screen

--- game/src/main/java/org/teamfarce/mirch/Screens/InterviewScreen.java
+++ game/src/main/java/org/teamfarce/mirch/Screens/InterviewScreen.java
@@ -1,22 +1,60 @@
 package org.teamfarce.mirch.Screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import org.teamfarce.mirch.MIRCH;
+import org.teamfarce.mirch.Screens.Elements.StatusBar;
+
 /**
  * Created by Jason on 13/02/2017.
  */
 public class InterviewScreen extends AbstractScreen {
+
+    private MIRCH game;
+
+    public Stage interviewStage;
+    private Skin uiSkin;
+    private StatusBar statusBar;
+
+    public InterviewScreen(MIRCH game, Skin uiSkin)
+    {
+        super(game);
+        this.game = game;
+        this.uiSkin = uiSkin;
+
+        statusBar = new StatusBar(game.gameSnapshot, uiSkin);
+
+        initStage();
+    }
+
+    private void initStage() {
+        //Initialise stage used to show interview contents
+        interviewStage = new Stage(new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+
+    }
+
     @Override
     public void show() {
-
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(interviewStage);
+        multiplexer.addProcessor(statusBar.stage);
+        Gdx.input.setInputProcessor(multiplexer);
     }
 
     @Override
     public void render(float delta) {
-
+        interviewStage.act();
+        interviewStage.draw();
+        statusBar.render();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        interviewStage.getViewport().update(width, height, false);
+        statusBar.resize(width, height);
     }
 
     @Override
@@ -36,6 +74,7 @@ public class InterviewScreen extends AbstractScreen {
 
     @Override
     public void dispose() {
-
+        interviewStage.dispose();
+        statusBar.dispose();
     }
 }
