3b46c3ee2d52f894bcad899596e851da42922a77
Brooke Hatton <hatton.brooke@gmail.com>
2017-02-16 17:18:33 +0000

    Revert "Attempt to fix tests"
    This reverts commit 764fb35f74a5d8470ba1b81c5768657dc02f1fc9.

--- game/src/test/java/org/teamfarce/mirch/Player_Test.java
+++ game/src/test/java/org/teamfarce/mirch/Player_Test.java
@@ -7,10 +7,6 @@ import org.teamfarce.mirch.GameTest;
 import org.teamfarce.mirch.Vector2Int;
 import org.teamfarce.mirch.map.Room;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 /**
@@ -36,18 +32,8 @@ public class Player_Test extends GameTest {
         p.setTileCoordinates(start);
         p.setRoom(new Room(0, "testMap.tmx", "name"));
 
-
-        Vector2Int c1 = new Vector2Int(0,0);
-        Vector2Int c2 = new Vector2Int(1,0);
-        Vector2Int c3 = new Vector2Int(2,0);
-        Vector2Int c4 = new Vector2Int(3,0);
-        Vector2Int c5 = new Vector2Int(4,0);
-        Vector2Int c6 = new Vector2Int(4,1);
-        Vector2Int c7 = new Vector2Int(4,2);
-        Vector2Int c8 = new Vector2Int(4,3);
-        Vector2Int c9 = new Vector2Int(4,4);
-        List<Vector2Int> list = Arrays.asList(c1,c2,c3,c4,c5,c6,c7,c8,c9);
-        assertEquals(list, p.aStarPath(dest));
+        String desiredSolution = "[(0,0), (1,0), (2,0), (3,0), (4,0), (4,1), (4,2), (4,3), (4,4)]";
+        assertEquals(desiredSolution, p.aStarPath(dest).toString());
     }
 
 }
