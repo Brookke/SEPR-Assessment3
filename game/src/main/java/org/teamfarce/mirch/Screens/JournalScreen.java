823f74f4240cce28702c60ef739872a567ed699f
Joe Shuff <joe.shuff@btinternet.com>
2017-02-18 15:01:57 +0000

    Added hover events to label in journal

--- game/src/main/java/org/teamfarce/mirch/Screens/JournalScreen.java
+++ game/src/main/java/org/teamfarce/mirch/Screens/JournalScreen.java
@@ -6,9 +6,11 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import org.teamfarce.mirch.Entities.Clue;
@@ -161,6 +163,25 @@ public class JournalScreen extends AbstractScreen
         for (int i = 0; i < clues.size(); i++) {
             Label clueLabel = new Label(clues.get(i).getName(), uiSkin);
             content.add(clueLabel).width(PAGE_CONTENT_WIDTH).height(30);
+
+            clueLabel.addListener(new ClickListener() {
+
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+
+                }
+
+                @Override
+                public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                    clueLabel.setStyle(uiSkin.get("white", Label.LabelStyle.class));
+                }
+
+                @Override
+                public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                    clueLabel.setStyle(uiSkin.get("default", Label.LabelStyle.class));
+                }
+            });
+
             content.row();
         }
 
