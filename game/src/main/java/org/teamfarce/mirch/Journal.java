946f68a90146df7325e2350bdf525689d134f882
Joe Shuff <joe.shuff@btinternet.com>
2017-02-18 23:47:50 +0000

    Refactor displayMotive()

--- game/src/main/java/org/teamfarce/mirch/Journal.java
+++ game/src/main/java/org/teamfarce/mirch/Journal.java
@@ -55,25 +55,33 @@ public class Journal
 
         if (motivesFound == 3)
         {
-            game.guiController.narratorScreen.setSpeech("Congratulations! You have solved the killers motive! Let's take a look at those clues...\n \n"
-                                                       + getMotive() + "\n \nI can't believe someone would kill someone for that!\n \n"
-                                                       + "Well, you go out there and find out who committed this murder!\n \nGood Luck!")
-                                            .setButton("Continue Game", new Runnable() {
-                                                @Override
-                                                public void run() {
-                                                    game.gameSnapshot.setState(GameState.map);
-                                                }
-                                            });
-
-            game.gameSnapshot.setState(GameState.narrator);
-
-            game.gameSnapshot.modifyScore(10);
+            displayMotive();
         }
 
         if (clue.isMeansClue())
         {
             foundWeapon = true;
         }
+    }
+
+    /**
+     * This method displays the complete motive to the player
+     */
+    private void displayMotive()
+    {
+        game.guiController.narratorScreen.setSpeech("Congratulations! You have solved the killers motive! Let's take a look at those clues...\n \n"
+                + getMotive() + "\n \nI can't believe someone would kill someone for that!\n \n"
+                + "Well, you go out there and find out who committed this murder!\n \nGood Luck!")
+                .setButton("Continue Game", new Runnable() {
+                    @Override
+                    public void run() {
+                        game.gameSnapshot.setState(GameState.map);
+                    }
+                });
+
+        game.gameSnapshot.setState(GameState.narrator);
+
+        game.gameSnapshot.modifyScore(10);
     }
 
     /**
