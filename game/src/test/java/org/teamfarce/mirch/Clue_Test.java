17c887c02943dee0ddb78a2cb8ddb7daf312c313
Joe Shuff <joe.shuff@btinternet.com>
2017-02-19 16:19:45 +0000

    Added tests

--- game/src/test/java/org/teamfarce/mirch/Clue_Test.java
+++ game/src/test/java/org/teamfarce/mirch/Clue_Test.java
@@ -3,7 +3,11 @@
  */
 package org.teamfarce.mirch;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.teamfarce.mirch.entities.Clue;
+
+import static org.junit.Assert.*;
 
 /**
  * Tests the clues class
@@ -13,20 +17,39 @@ import org.junit.Test;
 public class Clue_Test
 {
 
+    Clue clue;
+
+    @Before
+    public void before()
+    {
+        clue = new Clue("Test Clue", "Test Description", "clueSheet.png", 0, 0, false);
+    }
+
     /**
      * Test the initialiser function
      */
     @Test
-    public void test_init()
+    public void getInfo()
     {
-//		int provesMotive = 100;
-//		int provesMean = 50;
-//		String name = "test name";
-//
-//		Clue clue = new Clue(provesMotive, provesMean, name);
-//
-//		assertSame(provesMotive, clue.provesMotive);
-//		assertSame(provesMean, clue.provesMean);
-//		assertSame(name, clue.name);
+		assertEquals("Test Clue", clue.getName());
+		assertEquals("Text Description", clue.getDescription());
+		assertEquals(0, clue.getResourceX());
+		assertEquals(0, clue.getResourceY());
+    }
+
+    @Test
+    public void isMotive()
+    {
+        assertFalse(clue.isMotiveClue());
+        clue.setMotiveClue();
+        assertTrue(clue.isMotiveClue());
+    }
+
+    @Test
+    public void isMeans()
+    {
+        assertFalse(clue.isMeansClue());
+        clue = new Clue("Test Clue", "Test Description", "clueSheet.png", 0, 0, true);
+        assertTrue(clue.isMeansClue());
     }
 }
