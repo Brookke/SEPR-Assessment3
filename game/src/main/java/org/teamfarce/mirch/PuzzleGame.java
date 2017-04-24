2d11f0b584e9a46673a450d9967689fb83988a2d
jackson8en <jackson.8en@gmail.com>
2017-04-24 17:56:01 +0100

    Puzzle Game Complete
    Puzzle Game is complete, works well. Now we need to add the magic mirror
    bonus

--- game/src/main/java/org/teamfarce/mirch/PuzzleGame.java
+++ game/src/main/java/org/teamfarce/mirch/PuzzleGame.java
@@ -1,18 +1,22 @@
 package org.teamfarce.mirch;
 
+import org.teamfarce.mirch.Screens.PuzzleScreen;
+
 /**
  * Created by jacks on 17/03/2017.
  */
 public class PuzzleGame {
     /*
     *
-    * Public boolen for "game completed"
-    * method for teh game logic? linked to 8 buttons on screen
+    * Public boolean for "game completed"
+    * method for the game logic? linked to 8 buttons on screen
     * method updates bool to true or false
     * depending on if the game was won
     *
     */
     private boolean puzzleWon;
+    public String codeEntered = "CODE";
+    public String correctCode = "CODE42";
 
     public void PuzzleGame(){
         puzzleWon = false;
@@ -24,5 +28,14 @@ public class PuzzleGame {
 
     public void setPuzzleWon(){
         this.puzzleWon = true;
+    }
+
+    public void addToCode(String digit){
+        codeEntered += digit;
+        System.out.println(codeEntered);
+        if(codeEntered.contentEquals(correctCode)){
+            setPuzzleWon();
+
+        }
     }
 }
